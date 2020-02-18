32d2d856f62d00ce56f23ec327c2a6f2024a22fb
RowlandSanders <48600494+RowlandSanders@users.noreply.github.com>
2020-02-17 20:45:36 -0800

    Beginning generate random numbers

---
+++ RandomNum/src/cecs496/Main.java
@@ -0,0 +1,28 @@
+package cecs496;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    //Goal is generate random numbers using 11634 alg
+
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        ArrayList<Integer> middlenums = new ArrayList<Integer>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int ogvalue = input.nextInt();
+
+        System.out.print(("1234").substring(1,2));
+
+
+
+
+
+
+
+
+
+    }
+}
